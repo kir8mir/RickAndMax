a1941469af8171e71425ac7b60b23a0eeaf62dd1
Cyrill Prudchenko <49589547+kir8mir@users.noreply.github.com>
2022-12-09 18:48:45 +0200

    dfgdfg

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './index.scss';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './app/store';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { CharactersList } from './components/Characters/CharactersList';
 import { LocationList } from './components/Locations/LocationList';
@@ -37,4 +36,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
